30f1a199a8394ad932c8d3c10d37234c00d33563
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-06-02 20:36:05 +0300

    Implement data

--- components/Board.tsx
+++ components/Board.tsx
@@ -4,18 +4,23 @@ import React, { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
-  const getBoard = useBoardStore((state) => state.getBoard);
-
+  const [getBoard, board] = useBoardStore((state) => [state.getBoard, state.board]);
+  console.log(board);
   useEffect(() => {
     getBoard();
   }, [getBoard]);
   return (
-    <h1>hello</h1>
-    // <DragDropContext>
-    //   <Droppable droppableId="board" direction="horizontal" type="column">
-    //     {(provided: any) => <div></div>}
-    //   </Droppable>
-    // </DragDropContext>
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided: any) => (
+          <div {...provided.droppableProps} ref={provided.innerRef} className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto">
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
