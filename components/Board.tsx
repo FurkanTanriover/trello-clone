30cfd8fab4db7f9ab94c6da637911ed6312852e0
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-06-02 19:34:29 +0300

    Implementing zustand

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,21 +1,21 @@
 "use client";
-import React from "react";
+import { useBoardStore } from "@/store/BoardStore";
+import React, { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided: any) => (
-          <div
-            className="flex flex-1 overflow-x-scroll"
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-          >
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <h1>hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided: any) => <div></div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 };
 
