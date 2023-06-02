dd53d08c0815c271650e550241dfd6645d0e2e87
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-06-02 12:34:33 +0300

    Create Appwrite entegration

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
   return (
-    <div>Board</div>
-  )
-}
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided: any) => (
+          <div
+            className="flex flex-1 overflow-x-scroll"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
 
-export default Board
+export default Board;
