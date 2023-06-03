f97d9e6d6bd1fe52150204b63dc78fb0847ebb21
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-06-03 20:47:24 +0300

    Create drag&drop functionality

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,19 +1,90 @@
 "use client";
 import { useBoardStore } from "@/store/BoardStore";
 import React, { useEffect } from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+import Column from "./Column";
 
 const Board = () => {
-  const [getBoard, board] = useBoardStore((state) => [state.getBoard, state.board]);
+  const [getBoard, board, setBoardState, updateTodoInDB] = useBoardStore((state) => [
+    state.getBoard,
+    state.board,
+    state.setBoardState,
+    state.updateTodoInDB,
+  ]);
   console.log(board);
   useEffect(() => {
     getBoard();
   }, [getBoard]);
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+    if (!destination) return;
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const endColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+    const endCol: Column = {
+      id: endColIndex[0],
+      todos: endColIndex[1].todos,
+    };
+    if (!startCol || !endCol) return;
+
+    if (source.index === destination.index && startCol === endCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+    if (startCol.id === endCol.id) {
+      // same column task drag
+      const newColumns = new Map(board.columns);
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      newColumns.set(startCol.id, newCol);
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      //  dragging to another column
+      const endTodos = Array.from(endCol.todos);
+      endTodos.splice(destination.index, 0, todoMoved);
+      const newColumns = new Map(board.columns);
+      const newStartCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newEndCol = {
+        id: endCol.id,
+        todos: endTodos,
+      };
+      newColumns.set(startCol.id, newStartCol);
+      newColumns.set(endCol.id, newEndCol);
+      // update db
+      updateTodoInDB(todoMoved, endCol.id);
+      setBoardState({ ...board, columns: newColumns });
+    }
+  };
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
         {(provided: any) => (
-          <div {...provided.droppableProps} ref={provided.innerRef} className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto">
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
+          >
             {Array.from(board.columns.entries()).map(([id, column], index) => (
               <Column key={id} id={id} todos={column.todos} index={index} />
             ))}
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,9 +1,12 @@
+import { database } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupByColumn";
 import { create } from "zustand";
 
 interface BoardState {
   board: Board;
   getBoard: () => void;
+  setBoardState: (board: Board) => void;
+  updateTodoInDB: (todo: Todo, columnId: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -12,6 +15,18 @@ export const useBoardStore = create<BoardState>((set) => ({
   },
   getBoard: async () => {
     const board = await getTodosGroupedByColumn();
-    set({board})
+    set({ board });
+  },
+  setBoardState: (board) => set({ board }),
+  updateTodoInDB: async (todo, columnId) => {
+    await database.updateDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_TODOS_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    );
   },
 }));
