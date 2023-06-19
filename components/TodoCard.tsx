b08853bf710da4c9b7b55e7d283dcac01b708e9e
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2023-06-19 10:08:35 +0300

    Implement sweetalert2

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -7,6 +7,7 @@ import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
 } from "react-beautiful-dnd";
+import Swal from "sweetalert2";
 interface Props {
   todo: Todo;
   index: number;
@@ -19,6 +20,35 @@ interface Props {
 const TodoCard = ({ todo, index, id, innerRef, draggableProps, dragHandleProps }: Props) => {
   const deleteTask = useBoardStore((state) => state.deleteTask);
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const deleteTaskAlert = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#60A5FA",
+      cancelButtonColor: "#EF4444",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteTask(index, todo, id);
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your task has been deleted.",
+          icon: "success",
+          confirmButtonColor: "#60A5FA",
+        });
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: "Cancelled!",
+          text: "Your task is safe.",
+          icon: "error",
+          confirmButtonColor: "#60A5FA",
+        });
+      }
+    });
+  };
 
   useEffect(() => {
     if (todo.image) {
@@ -37,10 +67,7 @@ const TodoCard = ({ todo, index, id, innerRef, draggableProps, dragHandleProps }
       <div className="bg-white rounded-md space-y-2 drop-shadow-md">
         <div className="flex justify-between items-center p-2">
           <h3 className="font-bold text-lg">{todo.title}</h3>
-          <button
-            onClick={() => deleteTask(index, todo, id)}
-            className="text-red-500 hover:text-red-600"
-          >
+          <button onClick={() => deleteTaskAlert()} className="text-red-500 hover:text-red-600">
             <XCircleIcon className="h-8 w-8 ml-5" />
           </button>
         </div>
